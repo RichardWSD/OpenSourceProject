019985a012405b18cdd690fb94c656aad1362999
Richard <wushideng@aiyunxiao.com>
2020-12-05 00:26:36 +0800

    Dep example
    Signed-off-by: Richard <wushideng@aiyunxiao.com>

---
+++ vue-analysis/vue/my-example/my-project/src/App-Dep.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div v-if="flag">
+      {{ msg }}
+    </div>
+    <div v-else>
+      {{ msg1 }}
+    </div>
+    <button @click="change">change</button>
+    <button @click="toggle">toggle</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      flag: false,
+      msg: 'Hello World',
+      msg1: 'Hello Vue'
+    }
+  },
+  methods: {
+    change() {
+      this.msg = Math.random();
+    },
+    toggle() {
+      this.flag = !this.flag;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
