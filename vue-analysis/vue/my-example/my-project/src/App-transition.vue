329c084f81cf8b43abf209d5b210ca808cedccf6
Richard <wushideng@aiyunxiao.com>
2021-12-12 22:09:15 +0800

    m

---
+++ vue-analysis/vue/my-example/my-project/src/App-transition.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+  <button v-on:click="show = !show">
+    Toggle
+  </button>
+  <transition :appear="true" name="fade">
+  <p v-if="show">hello</p>
+  </transition>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+     show: true
+    }
+  },
+}
+</script>
+
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 2s;
+}
+.fade-enter, .fade-leave-to {
+  opacity: 0;
+}
+</style>
--- vue-analysis/vue/my-example/my-project/src/main.js
+++ vue-analysis/vue/my-example/my-project/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './App-props'
+import App from './App-transition-group'
 
 Vue.config.productionTip = false
 
