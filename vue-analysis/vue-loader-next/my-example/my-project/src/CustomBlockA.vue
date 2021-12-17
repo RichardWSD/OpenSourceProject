791f97842a3006487aa0ed2513f75ca54275cc08
richard <wushideng@ifanr.com>
2021-12-17 21:39:20 +0800

    m

---
+++ vue-analysis/vue-loader-next/my-example/my-project/src/CustomBlockA.vue
@@ -0,0 +1,20 @@
+<!-- ComponentA.vue -->
+<template>
+  <div>
+    <ComponentB/>
+    <p>{{ docs }}</p>
+  </div>
+</template>
+
+<script>
+import ComponentB from './CustomeBlockB.vue';
+
+export default {
+  components: { ComponentB },
+  data () {
+    return {
+      docs: ComponentB.__docs
+    }
+  }
+}
+</script>
---
+++ vue-analysis/vue-loader-next/my-example/my-project/src/components/Users.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="users">
+    <h1>Hello Users</h1>
+    <h2>slldllds</h2>
+    <ul>
+        <li v-for="user in users" :key="user">
+            {{user}}
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  data () {
+    return {
+     users:['Henry','Bucky','Emily']
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1{
+  color: green;
+}
+</style>
