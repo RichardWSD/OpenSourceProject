902ec215ebc0d466aae762be94a1f854f2c24082
richard <wushideng@ifanr.com>
2021-12-29 19:02:08 +0800

    m

--- vue-analysis/vue-loader-15.9.8/my-example/webpack-vue-loader/build/webpack.common.js
+++ vue-analysis/vue-loader-15.9.8/my-example/webpack-vue-loader/build/webpack.common.js
@@ -1,6 +1,6 @@
 const {join:pathJoin} = require('path');
 const CleanWebpackPlugin = require("clean-webpack-plugin");
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const VueLoaderPlugin = require('../../../lib/plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -16,12 +16,17 @@ module.exports = {
           chunks:'all' // +++ initial(初始块)、async(按需加载块)、all(全部块)
       }
     },
+    resolveLoader: {
+      alias: {
+        'vue-loader': require.resolve('../../../lib')
+      }
+    },
     plugins:[
         new HtmlWebpackPlugin({
             template:'./index.html',
             filename:'index.html'
         }),
         new VueLoaderPlugin(),
-        new CleanWebpackPlugin(['dist'])
+        new CleanWebpackPlugin()
     ]
 }
